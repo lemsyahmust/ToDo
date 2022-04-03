c1cf0479e1b55647036d75852662f364e94aa0c2
LEMSYAH Mustapha <83125801+lemsyahmust@users.noreply.github.com>
2022-04-03 21:51:28 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,29 @@
 from flask import Flask, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
+from flask_wtf import wtforms, FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, length, ValidationError, Length
 
 app = Flask(__name__)
 db = SQLAlchemy(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SECRET_KEY'] = 'thisisasecretkey'
 
+
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key = True)
-    username = db.Column(db.String(20), nullable = False)
+    username = db.Column(db.String(20), nullable = False, unique = True)
     password = db.Column(db.String(80), nullable = False)
+
+class RegisterForm(FlaskForm):
+    username = StringField(validators=[InputRequired(), Length(
+        min=4, max=20)], render_kw={"placeholder": "Username"})
+    password = PasswordField(validators=[InputRequired(), Length(
+        min = 4, max = 20)], render_kw={"placeholder": "Password"})
+
+    submit = SubmitField("Register")
+
 
 @app.route('/')
 def home():
